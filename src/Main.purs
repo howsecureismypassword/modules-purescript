835aa67758d6b4ea6025b5219e3b1fadcf69a631
Mark Wales <mark@smallhadroncollider.com>
2017-10-04 15:14:54 +0100

    added tests

--- src/Main.purs
+++ src/Main.purs
@@ -16,14 +16,4 @@ namedNumber' = namedNumber names
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log $ namedNumber' 12.0
-  log $ namedNumber' 220.0
-  log $ namedNumber' 3200.0
-  log $ namedNumber' 42000.0
-  log $ namedNumber' 520000.0
-  log $ namedNumber' 6200000.0
-  log $ namedNumber' 72000000.0
   log $ namedNumber' 820000000.0
-  log $ namedNumber' 9200000000.0
-  log $ namedNumber' 12000000000.0
-  log $ namedNumber' 220000000000.0
--- test/Main.purs
+++ test/Main.purs
@@ -2,8 +2,26 @@ module Test.Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (RunnerEffects, run)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+import Main(namedNumber')
+
+main :: Eff (RunnerEffects ()) Unit
+main = run [consoleReporter] do
+    describe "namedNumber'" do
+       it "names numbers" do
+          namedNumber' 12.0 `shouldEqual` "12"
+          namedNumber' 220.0 `shouldEqual` "2 hundred"
+          namedNumber' 3200.0 `shouldEqual` "3 thousand"
+          namedNumber' 42000.0 `shouldEqual` "42 thousand"
+          namedNumber' 520000.0 `shouldEqual` "5 hundred thousand"
+          namedNumber' 6200000.0 `shouldEqual` "6 million"
+          namedNumber' 72000000.0 `shouldEqual` "72 million"
+          namedNumber' 820000000.0 `shouldEqual` "8 hundred million"
+          namedNumber' 9200000000.0 `shouldEqual` "9 billion"
+          namedNumber' 12000000000.0 `shouldEqual` "12 billion"
+          namedNumber' 220000000000.0 `shouldEqual` "2 hundred billion"
+
