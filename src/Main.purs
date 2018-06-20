399ab631254dfa6114b7268045c73c87dedfb905
Mark Wales <mark@smallhadroncollider.com>
2018-06-20 11:24:58 +0100

    chore (Main): updated to PureScript 0.12

--- src/Main.purs
+++ src/Main.purs
@@ -1,8 +1,8 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Effect (Effect)
+import Effect.Console (log)
 
 -- named number logic
 import NamedNumber (namedNumber)
@@ -22,6 +22,6 @@ namedNumber' = namedNumber names
 calculate' :: String -> Number
 calculate' = calculate sets 
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main :: Effect Unit
 main = do
   log $ namedNumber' $ calculate' "hello" 
