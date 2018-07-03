ee03c2420c1f02ed19660c3f552bef32141062d9
Mark Wales <mark@smallhadroncollider.com>
2018-07-03 12:09:37 +0100

    feat (Makefile): added Makefile

---
+++ Makefile
@@ -0,0 +1,13 @@
+.PHONY: all
+
+all: build.js build.min.js
+
+output/%.js:
+	pulp build
+
+build.js: output/Calculator/index.js output/Checker/index.js output/Main/index.js output/NamedNumber/index.js output/Period/index.js output/Utility/index.js output/Checks.Dictionary/index.js output/Checks.Pattern/index.js output/Checks.Patterns/index.js
+	pulp build --skip-entry-point -t build.js
+	echo "module.exports = PS.Main.setup" >> build.js
+
+build.min.js: build.js
+	uglifyjs -m -- build.js > build.min.js
--- test.js
+++ test.js
@@ -1,10 +1,18 @@
-const setup = require("./output/Main").setup;
+const setup = require("./build.min");
 
-console.log(setup({
+const hsimp = setup({
     calcs: 40e9,
     periods: require("./dictionaries/periods"),
     namedNumbers: require("./dictionaries/named-numbers"),
     characterSets: require("./dictionaries/character-sets"),
     dictionary: require("./dictionaries/top10k"),
     patterns: require("./dictionaries/patterns"),
-})("HowSecureIsMyPassword?"))
+});
+
+console.time();
+
+for (let i = 0; i < 1000; i++) {
+    hsimp("HowSecureIsMyPassword?");
+}
+
+console.timeEnd();
