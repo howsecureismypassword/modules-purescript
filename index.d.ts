b4c123f31024e4e42543e52c9f92c97901185258
icepy <xiangwenwe@foxmail.com>
2019-04-16 19:50:26 +0800

    [add] update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ interface IChecks {
     level: string;
 }
 
-interface IResult {
+export interface IResult {
     time: string;
     level: string;
     checks: IChecks[];
@@ -12,5 +12,69 @@ interface IResult {
 
 type returnFunction = (m: string | number) => IResult;
 
-// TODO config Type
-export default function setup(config: any): returnFunction;
+interface ICharacterSet {
+    name: string;
+    matches: string;
+    value: value;
+}
+
+type CharacterSets = ICharacterSet[];
+
+interface ICalculation {
+    calcs: number;
+    characterSets: CharacterSets;
+}
+
+interface IPeriod {
+    singular: string;
+    plural: string;
+    seconds: number;
+}
+
+type Periods = IPeriod[];
+
+interface INameNumber {
+    name: string;
+    value: number;
+}
+
+type NamedNumbers = INameNumber[];
+
+interface ITime {
+    periods: Periods;
+    namedNumbers: NamedNumbers;
+    forever: string;
+    instantly: string;
+}
+
+type Dictionary = string[];
+
+interface IPattern {
+    level: string;
+    id: string;
+    regex: string;
+}
+
+type Patterns = IPattern[];
+
+interface IMessage {
+    id: string;
+    name: string;
+    message: string;
+}
+
+type Messages = IMessage[];
+
+interface IChecks {
+    dictionary: Dictionary;
+    patterns: Patterns;
+    messages: Messages;
+}
+
+export interface ISetupConfig {
+    calculation: ICalculation;
+    time: ITime;
+    checks: IChecks;
+}
+
+export default function setup(config: ISetupConfig): returnFunction;
