ae861180a6c31355b89b24b7ffe700a3436cf3ef
icepy <xiangwenwe@foxmail.com>
2019-04-16 20:43:38 +0800

    [fix] fix commonjs module

--- index.d.ts
+++ index.d.ts
@@ -1,80 +1,84 @@
-interface IChecks {
-    name: string;
-    message: string;
-    level: string;
+declare module 'hsimp-purescript' {
+    interface IChecks {
+      name: string;
+      message: string;
+      level: string;
+    }
+
+    interface IResult {
+      time: string;
+      level: string;
+      checks: IChecks[];
+    }
+
+    type returnFunction = (m: string | number) => IResult;
+
+    interface ICharacterSet {
+      name: string;
+      matches: string;
+      value: number;
+    }
+
+    type CharacterSets = ICharacterSet[];
+
+    interface ICalculation {
+      calcs: number;
+      characterSets: CharacterSets;
+    }
+
+    interface IPeriod {
+      singular: string;
+      plural: string;
+      seconds: number;
+    }
+
+    type Periods = IPeriod[];
+
+    interface INameNumber {
+      name: string;
+      value: number;
+    }
+
+    type NamedNumbers = INameNumber[];
+
+    interface ITime {
+      periods: Periods;
+      namedNumbers: NamedNumbers;
+      forever: string;
+      instantly: string;
+    }
+
+    type Dictionary = string[];
+
+    interface IPattern {
+      level: string;
+      id: string;
+      regex: string;
+    }
+
+    type Patterns = IPattern[];
+
+    interface IMessage {
+      id: string;
+      name: string;
+      message: string;
+    }
+
+    type Messages = IMessage[];
+
+    interface IChecks {
+      dictionary: Dictionary;
+      patterns: Patterns;
+      messages: Messages;
+    }
+
+    interface ISetupConfig {
+      calculation: ICalculation;
+      time: ITime;
+      checks: IChecks;
+    }
+
+    function setup(config: ISetupConfig): returnFunction;
+
+    export = setup;
 }
-
-export interface IResult {
-    time: string;
-    level: string;
-    checks: IChecks[];
-}
-
-type returnFunction = (m: string | number) => IResult;
-
-interface ICharacterSet {
-    name: string;
-    matches: string;
-    value: number;
-}
-
-type CharacterSets = ICharacterSet[];
-
-interface ICalculation {
-    calcs: number;
-    characterSets: CharacterSets;
-}
-
-interface IPeriod {
-    singular: string;
-    plural: string;
-    seconds: number;
-}
-
-type Periods = IPeriod[];
-
-interface INameNumber {
-    name: string;
-    value: number;
-}
-
-type NamedNumbers = INameNumber[];
-
-interface ITime {
-    periods: Periods;
-    namedNumbers: NamedNumbers;
-    forever: string;
-    instantly: string;
-}
-
-type Dictionary = string[];
-
-interface IPattern {
-    level: string;
-    id: string;
-    regex: string;
-}
-
-type Patterns = IPattern[];
-
-interface IMessage {
-    id: string;
-    name: string;
-    message: string;
-}
-
-type Messages = IMessage[];
-
-interface IChecks {
-    dictionary: Dictionary;
-    patterns: Patterns;
-    messages: Messages;
-}
-
-export interface ISetupConfig {
-    calculation: ICalculation;
-    time: ITime;
-    checks: IChecks;
-}
-
-export default function setup(config: ISetupConfig): returnFunction;
