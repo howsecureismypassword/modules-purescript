f4edf364d366209659ee12933f6a4652766e9f1a
Mark Wales <mark@smallhadroncollider.com>
2020-07-01 11:00:55 +0100

    refactor: cleans up `findLast`

--- src/Utility.purs
+++ src/Utility.purs
@@ -3,21 +3,18 @@ module Utility (
   , format
 ) where
 
-import Data.List.NonEmpty (NonEmptyList, fromFoldable, head, tail)
-import Data.Maybe (maybe)
+import Prelude ((=<<), (<<<), not)
+import Data.List.NonEmpty (NonEmptyList, head, findLastIndex, index)
+import Data.Maybe (fromMaybe)
 
 import Data.String.Regex (Regex, replace)
 import Data.String.Regex.Unsafe (unsafeRegex)
 import Data.String.Regex.Flags (global)
 
 -- find last
-findLast' :: forall a. (a -> Boolean) -> a -> NonEmptyList a -> a
-findLast' fn prev list = if fn x then prev else maybe x (findLast' fn x) xs
-    where x = head list
-          xs = fromFoldable (tail list)
-
 findLast :: forall a. (a -> Boolean) -> NonEmptyList a -> a
-findLast fn list = findLast' fn (head list) list
+findLast fn list = fromMaybe (head list) last
+        where last = index list =<< findLastIndex (not <<< fn) list
 
 -- number formatting
 formatRegex :: Regex
