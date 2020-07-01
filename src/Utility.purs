cfabcc7a8b29ddcdd14a0f54379d1b3324195dd0
Mark Wales <mark@smallhadroncollider.com>
2020-07-01 10:38:36 +0100

    feat: adds number formatting

--- src/Utility.purs
+++ src/Utility.purs
@@ -1,10 +1,16 @@
 module Utility (
     findLast
+  , format
 ) where
 
 import Data.List.NonEmpty (NonEmptyList, fromFoldable, head, tail)
 import Data.Maybe (maybe)
 
+import Data.String.Regex (Regex, replace)
+import Data.String.Regex.Unsafe (unsafeRegex)
+import Data.String.Regex.Flags (global)
+
+-- find last
 findLast' :: forall a. (a -> Boolean) -> a -> NonEmptyList a -> a
 findLast' fn prev list = if fn x then prev else maybe x (findLast' fn x) xs
     where x = head list
@@ -12,3 +18,10 @@ findLast' fn prev list = if fn x then prev else maybe x (findLast' fn x) xs
 
 findLast :: forall a. (a -> Boolean) -> NonEmptyList a -> a
 findLast fn list = findLast' fn (head list) list
+
+-- number formatting
+formatRegex :: Regex
+formatRegex = unsafeRegex "(\\d)(?=(\\d\\d\\d)+(?!\\d))" global
+
+format :: String -> String
+format number = replace formatRegex "$1," number
--- test/Utility.purs
+++ test/Utility.purs
@@ -8,7 +8,7 @@ import Data.List.NonEmpty (NonEmptyList, fromFoldable, singleton)
 import Data.Maybe (fromMaybe)
 
 -- tested modules
-import Utility (findLast)
+import Utility (findLast, format)
 
 singletonList :: NonEmptyList Int
 singletonList = singleton 1
@@ -20,3 +20,14 @@ checks = describe "Utility (findLast)" do
        findLast (\x -> x > 5) (fromMaybe singletonList (fromFoldable [1, 2, 3, 4, 9, 10])) `shouldEqual` 4
        findLast (\x -> x > 9) (fromMaybe singletonList (fromFoldable [1, 2, 3, 4, 9, 10])) `shouldEqual` 9
        findLast (\x -> x > 10) (fromMaybe singletonList (fromFoldable [1, 2, 3, 4, 9, 10])) `shouldEqual` 10
+
+    it "formats numbers" do
+       format "1" `shouldEqual` "1"
+       format "12" `shouldEqual` "12"
+       format "123" `shouldEqual` "123"
+       format "1234" `shouldEqual` "1,234"
+       format "12345" `shouldEqual` "12,345"
+       format "123456" `shouldEqual` "123,456"
+       format "1234567" `shouldEqual` "1,234,567"
+       format "12345.67" `shouldEqual` "12,345.67"
+       format "12345678909876543211234567890987654321" `shouldEqual` "12,345,678,909,876,543,211,234,567,890,987,654,321"
